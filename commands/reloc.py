3279e18a88601b0bab3324b03ba1a8fb9edcd633
yurich-official <PC.yurich@gmail.com>
2024-09-07 20:26:32 +0900

    1.3 - added `archive_in parameter and its description to reloc.py

--- commands/reloc.py
+++ commands/reloc.py
@@ -34,11 +34,29 @@ def get_parent(thread):
     else:
         return thread
 
+archive_duration_syntaxes = {
+    60: ["h", "1h", "hour", "1hour"],
+    1440: ["d", "1d", "day", "1day"],
+    4320: ["3d", "3days", "3day"],
+    10080: ["7d", "7days", "7day", "w", "week", "1week"],
+}
+
+def archive_duration_to_minutes(archive_in: str):
+    try:
+        return int(archive_in)
+    except ValueError:
+        pass
+    for minutes, syntaxes in archive_duration_syntaxes.items():
+        if archive_in in syntaxes:
+            return minutes
+    raise ValueError(f"invalid archive_in: {archive_in}")
+
 async def func(ctx,
-                flags: str | None = param(description=params['flags']),
-                thread_name: str | None = param(description=params['thread_name']),
-                start_date: str | None = param(description=params['start_date']),
-                end_date: str | None = param(description=params['end_date'])):
+               flags: str | None = param(description=params['flags']),
+               thread_name: str | None = param(description=params['thread_name']),
+               start_date: str | None = param(description=params['start_date']),
+               end_date: str | None = param(description=params['end_date']),
+               archive_in: str | None = param(description=params['archive_in'], default="60")):
     if flags == None:
         await flags_missing(ctx)
         return
@@ -47,7 +65,7 @@ async def func(ctx,
     except ValueError:
         return
     if flags['help']:
-        await conditional_log(ctx, flags, logs['h'], important=True)
+        await conditional_log(ctx, flags, logs['-h'], important=True)
         return
     if thread_name is None:
         await conditional_log(ctx, flags, logs['no-thread'], important=True)
@@ -63,12 +81,18 @@ async def func(ctx,
         await conditional_log(ctx, flags, logs['invalid-date'], important=True)
         return
     
+    try:
+        archive_in = archive_duration_to_minutes(archive_in)
+    except:
+        await conditional_log(ctx, flags, logs['invalid-archive-in'], important=True)
+        return
+    
     channel = get_parent(ctx.channel)
     if thread_name == "-":
         thread = channel
         await conditional_log(ctx, flags, logs['using-channel'].format(channel.mention))
     else:
-        thread = await channel.create_thread(name=thread_name, auto_archive_duration=60)
+        thread = await channel.create_thread(name=thread_name, auto_archive_duration=archive_in)
         await conditional_log(ctx, flags, logs['create-thread'].format(thread.mention))
 
     # Fetch messages from the channel
